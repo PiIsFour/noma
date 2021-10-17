9ff99e1f85d00b89747e403025a9a0a4c897d866
PiIsFour <PiIsFour@users.noreply.github.com>
2021-10-17 11:39:08 +0200

    extend getQuestion with subject

--- src/application/useCase/getQuestion.test.ts
+++ src/application/useCase/getQuestion.test.ts
@@ -1,7 +1,36 @@
+import { fromNullable, some } from 'fp-ts/Option'
+import { PartsOfSpeech, Word } from '../../domain/word'
+import { WordRepo } from '../ports/wordsRepo'
+
 import { getQuestion } from './getQuestion'
 
 describe('getQuestion', () => {
 	it('returns さくらが歩く。', () => {
-		expect(getQuestion()).toEqual('さくらが歩く。')
+		const sakura: Word = {
+			hiragana: 'さくら',
+			kanji: 'さくら',
+			translation: 'sakura',
+			partsOfSpeech: [PartsOfSpeech.noun],
+		}
+		const walk: Word = {
+			hiragana: 'あるく',
+			kanji: '歩く',
+			translation: 'walk',
+			partsOfSpeech: [PartsOfSpeech.verb],
+		}
+		const wordRepoMock: WordRepo = {
+			getWord: jest.fn(filter => fromNullable([sakura, walk].find(w => filter(w.partsOfSpeech)))),
+		}
+		expect(getQuestion(wordRepoMock)()).toEqual(some({
+			kanji: 'さくらが歩く。',
+			sentence: {
+				parts: [
+					{
+						type: 'ga',
+						word: sakura,
+					},
+				],
+			},
+		}))
 	})
 })
--- src/application/useCase/getQuestion.ts
+++ src/application/useCase/getQuestion.ts
@@ -1,1 +1,27 @@
-export const getQuestion = (): string => 'さくらが歩く。'
+import { pipe } from 'fp-ts/function'
+import { ap, Option, some } from 'fp-ts/Option'
+import { CarriageType, Sentence } from '../../domain/sentence'
+import { Word } from '../../domain/word'
+import { WordRepo } from '../ports/wordsRepo'
+
+interface Question {
+	kanji: string,
+	sentence: Sentence
+}
+
+export const getQuestion = (wordRepo: WordRepo) => (): Option<Question> => {
+	return pipe(
+		some((sakura: Word) => ({
+			kanji: 'さくらが歩く。',
+			sentence: {
+				parts: [
+					{
+						type: CarriageType.ga,
+						word: sakura,
+					},
+				],
+			},
+		})),
+		ap(wordRepo.getWord(() => true)),
+	)
+}
