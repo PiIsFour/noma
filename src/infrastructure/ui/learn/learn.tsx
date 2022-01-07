d24d6d1f801a3c3c22bc4b88ce6b4325cedd879c
PiIsFour <PiIsFour@users.noreply.github.com>
2022-01-07 16:56:19 +0100

    display the answer

--- src/infrastructure/ui/learn/learn.tsx
+++ src/infrastructure/ui/learn/learn.tsx
@@ -1,20 +1,40 @@
-import { pipe } from 'fp-ts/lib/function'
+import { flow } from 'fp-ts/lib/function'
 import { fold } from 'fp-ts/lib/Option'
 import React, { useEffect, useState } from 'react'
 import { fakeLessonRepo } from '../../../application/ports/fakeLessonRepo'
 import { fakeWordRepo } from '../../../application/ports/fakeWordRepo'
 import { getQuestion, Question as QuestionType } from '../../../application/useCase/getQuestion'
+import { Answer } from '../answer/answer'
+import { Button } from '../button/button'
 import { Question } from '../question/question'
+
+interface StateParams {
+	transitionTo: (stage: Stage) => void
+	nextQuestion: () => void
+}
+
+interface Controls {
+	text: string
+	action: () => void
+}
 
 class Loading {
 	render(): JSX.Element{
 		return <div>loading next lesson</div>
+	}
+
+	controls(): Controls[]{
+		return []
 	}
 }
 
 class Error {
 	render(): JSX.Element{
 		return <div>Error</div>
+	}
+
+	controls(): Controls[]{
+		return []
 	}
 }
 
@@ -23,24 +43,62 @@ class Ask {
 	render(): JSX.Element{
 		return <Question kanji={this.question.kanji} />
 	}
+
+	controls({ transitionTo }: StateParams): Controls[]{
+		return [
+			{
+				text: 'show',
+				action: () => transitionTo(new Review(this.question)),
+			},
+		]
+	}
 }
 
-type Stage = Loading | Error | Ask
+class Review {
+	constructor(private question: QuestionType){}
+	render(): JSX.Element{
+		const { kanji, sentence } = this.question
+		return <Answer kanji={kanji} sentence={sentence} />
+	}
 
-const nextQuestion = getQuestion(fakeWordRepo, fakeLessonRepo)
+	controls({ nextQuestion }: StateParams): Controls[]{
+		return [
+			{
+				text: 'next',
+				action: nextQuestion,
+			},
+		]
+	}
+}
+
+type Stage = Loading | Error | Ask | Review
+
+const lookUpQuestion = getQuestion(fakeWordRepo, fakeLessonRepo)
 
 export const Learn = (): JSX.Element => {
 	const [stage, setStage] = useState<Stage>(new Loading())
 
-	useEffect(() => {
-		pipe(
-			nextQuestion(),
-			fold(
-				() => setStage(new Error()),
-				question => setStage(new Ask(question)),
-			),
-		)
-	}, [])
+	const nextQuestion = flow(
+		lookUpQuestion,
+		fold(
+			() => setStage(new Error()),
+			question => setStage(new Ask(question)),
+		),
+	)
 
-	return stage.render()
+	useEffect(nextQuestion, [])
+
+	return <>
+		{stage.render()}
+		<div style={{
+			marginTop: '2rem',
+		}}>
+			{stage.controls({
+				transitionTo: setStage,
+				nextQuestion,
+			}).map(({ text, action }, index) =>
+				<Button key={index} label={text} onClick={action} />,
+			)}
+		</div>
+	</>
 }
