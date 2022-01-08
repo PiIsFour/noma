e2a8183e6b00b1a1b36a0156c5d6578f3c2f35cd
PiIsFour <PiIsFour@users.noreply.github.com>
2022-01-08 16:30:02 +0100

    clean up WordRepo

--- src/application/ports/wordsRepo.ts
+++ src/application/ports/wordsRepo.ts
@@ -1,6 +1,8 @@
 import { Option } from 'fp-ts/Option'
 import { PartsOfSpeech, Word } from '../../domain/word'
 
+type Filter = (partsOfSpeech: PartsOfSpeech[]) => boolean
+
 export interface WordRepo {
-	getWord(filter: (partsOfSpeech: PartsOfSpeech[]) => boolean): Option<Word>
+	getWord(filter: Filter): Option<Word>
 }
